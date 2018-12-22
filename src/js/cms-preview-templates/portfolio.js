d6c8b0bf0b8e4307ff19bfdce0d2f479bf77c543
Shishak <semenshishak@gmail.com>
2018-12-22 06:38:58 +0200

    Update portfolio.js

--- src/js/cms-preview-templates/portfolio.js
+++ src/js/cms-preview-templates/portfolio.js
@@ -18,7 +18,7 @@ const MediaBlock = ({heading, text, imageUrl, reverse}) => {
   </div>;
 };
 
-export default class ValuesPreview extends React.Component {
+export default class PortfolioPreview extends React.Component {
   render() {
     const {entry, getAsset} = this.props;
     
@@ -29,8 +29,8 @@ export default class ValuesPreview extends React.Component {
       image = window.parent.location.protocol + "//" + window.parent.location.host + image;
     }
     
-    const entryValues = entry.getIn(["data", "values"]);
-    const values = entryValues ? entryValues.toJS() : [];
+    const entryPortfolio = entry.getIn(["data", "potfolio"]);
+    const potfolio= entryPortfolio ? entryPortfolio.toJS() : [];
     
     return <div>
       <Jumbotron image={image} title={entry.getIn(["data", "title"])} />
